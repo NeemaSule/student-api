5b2d14a6148771679eea5879fecfe16cc76e26c1
NeemaSule <neemas.lulyeho@gmail.com>
2025-03-29 18:08:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.json());
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'student',
+    password: 'Neema@@24',
+    database: 'student_db'
 });
 
 db.connect((err) => {
