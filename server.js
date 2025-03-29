f3fdd110c924a0d289a19f6b6094f1cfaee256a1
NeemaSule <neemas.lulyeho@gmail.com>
2025-03-29 18:39:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -59,8 +59,8 @@ app.post("/students", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 
